673c59f0e19f98f08323e17544c144354092cb44
marconiryan <tencatenryan11@gmail.com>
2022-07-06 01:54:37 -0300

    first

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var string = "Um novo olhar sobre o futuro";
+var array = string.split("");
+var timer;
+
+function frameLooper () {
+	if (array.length > 0) {
+		document.getElementById("text").innerHTML += array.shift();
+	} else {
+		clearTimeout(timer);
+			}
+	loopTimer = setTimeout('frameLooper()',110);
+
+}
+frameLooper();
